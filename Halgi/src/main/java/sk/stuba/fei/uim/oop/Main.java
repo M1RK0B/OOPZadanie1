4b34a816e736d8dfc1d4cd4531921e6f04af5ec9
Miroslav Beňušík <xbenusik@stuba.sk>
2023-02-22 20:18:24 +0100

    jou

--- Halgi/src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ Halgi/src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,21 @@
 package sk.stuba.fei.uim.oop;
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        int a = 10;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("1");
+        } else if (a < b) {
+            System.out.println("-1");
+        } else {
+            System.out.println("0");
+        }
+
+
     }
 }
